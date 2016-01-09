6f47589140757b7b23203c86233f1f745a6d01f8
Kenichi Nakamura <nkkn1008@gmail.com>
2016-01-09 18:24:47 +0900

    Initialize

---
+++ DeviceSetting/Model/Printer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Printing;
+namespace DeviceSetting.Model
+{
+    class Printer
+    {
+        public Printer()
+        {
+            LocalPrintServer server = new LocalPrintServer();
+            var queues = server.GetPrintQueues();            
+            var queList = queues.
+                Select(x => x.FullName).
+                ToList();
+            printers = new ObservableCollection<string>(queList);
+                                
+        }
+        public string PrinterName { get; set; }
+        public ObservableCollection<string> printers { get; set; }
+    }
+}
---
+++ DeviceSetting/ViewModel/PrinterViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using DeviceSetting.Model;
+
+namespace DeviceSetting.ViewModel
+{
+    class PrinterViewModel : INotifyPropertyChanged
+    {
+        private Printer printer;
+        private ObservableCollection<string> printers;
+        public PrinterViewModel()
+        {
+            this.printer = new Printer();
+            this.printers = printer.printers;
+        }        
+        
+        public string FullName
+        {
+            get { return printer.PrinterName; }
+            set
+            {
+                printer.PrinterName = value;
+                NotifyPropertyChanged("FullName");
+            }
+        }
+
+
+        public ObservableCollection<string> PrinterList
+        {
+            get { return this.printers; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string v)
+        {            
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(v));
+            }
+        }
+    }
+}
