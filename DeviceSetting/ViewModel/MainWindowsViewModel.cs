47d5ae870cf44c19ee3d8ce9bdd6d1506ef10410
Kenichi Nakamura <nkkn1008@gmail.com>
2016-01-13 07:07:02 +0900

    ICommandを実装したclassをViewModelのクラスの外に出した

--- DeviceSetting/ViewModel/MainWindowsViewModel.cs
+++ DeviceSetting/ViewModel/MainWindowsViewModel.cs
@@ -56,19 +56,19 @@ namespace DeviceSetting.ViewModel
             }
         }
 
-        class OkCommandImpl : ICommand
-        {
-            public bool CanExecute(object parameter) { return true; }
-            public event EventHandler CanExecuteChanged;
-
-            public void Execute(object parameter)
-            {
-                MessageBox.Show("コマンドが実行されました。");
-            }
-        }
-
         public ICommand OkCommand { get; private set; }
 
+    }
+
+    class OkCommandImpl : ICommand
+    {
+        public bool CanExecute(object parameter) { return true; }
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            MessageBox.Show("コマンドが実行されました。");
+        }
     }
 }
 
